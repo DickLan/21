14a706e1da140b17027f9c690f462d7110686e76
Sea <pokemaster556610@gmail.com>
2022-08-31 11:08:18 +0800

    work okay version, already establish func enemyGetCards

--- 21/FirstViewController.swift
+++ 21/FirstViewController.swift
@@ -114,58 +114,8 @@ class FirstViewController: UIViewController {
         if bangLabel.isHidden == true{
             for _ in 1...i{
                 if enemyScore < 21 {
-                    // tempCard = String
-                    let tempCard:String? = cards.randomElement()
-                    if let tempCard = tempCard {
-                        enemyCards.append(tempCard)
-                        enemyCardsImageView[enemyGenerateTimes].image=UIImage(named: tempCard)
-                        print(tempCard)
-                        enemyGenerateTimes += 1
-
-                    if tempCard.description.contains("A"){
-                        enemyScore += 1
-                        enemyScoreLabel.text = String(enemyScore)
-                    }else if tempCard.description.contains("2"){
-                        enemyScore += 2
-                    }else if tempCard.description.contains("3"){
-                        enemyScore += 3
-                    }else if tempCard.description.contains("4"){
-                        enemyScore += 4
-                    }else if tempCard.description.contains("5"){
-                        enemyScore += 5
-                    }else if tempCard.description.contains("6"){
-                        enemyScore += 6
-                    }else if tempCard.description.contains("7"){
-                        enemyScore += 7
-                    }else if tempCard.description.contains("8"){
-                        enemyScore += 8
-                    }else if tempCard.description.contains("9"){
-                        enemyScore += 9
-                    }else if tempCard.description.contains("10"){
-                        enemyScore += 10
-                    }else if tempCard.description.contains("J"){
-                        enemyScore += 11
-                    }else if tempCard.description.contains("Q"){
-                        enemyScore += 12
-                    }else if tempCard.description.contains("K"){
-                        enemyScore += 13
-                    }else{
-                        print("error")
-                    }
-                    print("labelChange")
-                    enemyScoreLabel.text = String(enemyScore)
+                    enemyGetCards()
                 }
-                    
-                    //if let close
-                    if enemyScore > 21{
-                        print("bang")
-                        enemyBangLabel.isHidden = false
-                        return
-                }
-                
-                
-                }
-
         }
             
             
