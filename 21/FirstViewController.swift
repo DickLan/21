90f7d050ef0901ec706d297cda76f6e4da3fe8f8
Sea <pokemaster556610@gmail.com>
2022-08-31 11:04:01 +0800

    work okay base version

--- 21/FirstViewController.swift
+++ 21/FirstViewController.swift
@@ -11,7 +11,7 @@ class FirstViewController: UIViewController {
 
     //public
     var cards = [String]()
-    
+    var timer:Timer?
     @IBOutlet weak var resultLabel: UILabel!
     //mine
     
@@ -109,31 +109,75 @@ class FirstViewController: UIViewController {
 
         // give random cards to enemy
         let i = Int.random(in: 1...5)
-        
         //還沒爆掉時
-        if bangLabel.isHidden == true{
-        //for _ in 1...i{
-            let timer:Timer?
-            timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { timer in
-                self.enemyGetCards()
-            })
-            let count = Int.random(in: 1...5)
-            
 
+        if bangLabel.isHidden == true{
+            for _ in 1...i{
+                if enemyScore < 21 {
+                    // tempCard = String
+                    let tempCard:String? = cards.randomElement()
+                    if let tempCard = tempCard {
+                        enemyCards.append(tempCard)
+                        enemyCardsImageView[enemyGenerateTimes].image=UIImage(named: tempCard)
+                        print(tempCard)
+                        enemyGenerateTimes += 1
+
+                    if tempCard.description.contains("A"){
+                        enemyScore += 1
+                        enemyScoreLabel.text = String(enemyScore)
+                    }else if tempCard.description.contains("2"){
+                        enemyScore += 2
+                    }else if tempCard.description.contains("3"){
+                        enemyScore += 3
+                    }else if tempCard.description.contains("4"){
+                        enemyScore += 4
+                    }else if tempCard.description.contains("5"){
+                        enemyScore += 5
+                    }else if tempCard.description.contains("6"){
+                        enemyScore += 6
+                    }else if tempCard.description.contains("7"){
+                        enemyScore += 7
+                    }else if tempCard.description.contains("8"){
+                        enemyScore += 8
+                    }else if tempCard.description.contains("9"){
+                        enemyScore += 9
+                    }else if tempCard.description.contains("10"){
+                        enemyScore += 10
+                    }else if tempCard.description.contains("J"){
+                        enemyScore += 11
+                    }else if tempCard.description.contains("Q"){
+                        enemyScore += 12
+                    }else if tempCard.description.contains("K"){
+                        enemyScore += 13
+                    }else{
+                        print("error")
+                    }
+                    print("labelChange")
+                    enemyScoreLabel.text = String(enemyScore)
+                }
+                    
+                    //if let close
+                    if enemyScore > 21{
+                        print("bang")
+                        enemyBangLabel.isHidden = false
+                        return
+                }
+                
+                
+                }
+
+        }
             
             
             
         //比較雙方牌分
-            if generateTimes == count{
-                if myScore > enemyScore{
-                    resultLabel.text = "你淫惹"
-                    resultLabel.isHidden = false
-                }else{
-                    resultLabel.text = "對手淫惹"
-                    resultLabel.isHidden = false
-                }
-            }
-
+        if myScore > enemyScore{
+            resultLabel.text = "你淫惹"
+            resultLabel.isHidden = false
+        }else{
+            resultLabel.text = "對手淫惹"
+            resultLabel.isHidden = false
+        }
             
         }
         
